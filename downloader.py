e76c80d3d6c039df58c258637fd1b93f1adcce2e
Your Name <you@example.com>
2020-10-14 04:03:31 +0000

    VM Commit

--- downloader.py
+++ downloader.py
@@ -1,10 +1,12 @@
+import math
+import hashlib
 import argparse
 import shutil
 import threading
 import urllib.request
 import requests
 import sys, os
-
+import time
 # Author: Josh Messitte (811976008)
 # CSCI 6760 Project 3: download-accelerator
 # Test downloader: python3 downloader.py -n num_chunks -o output_dir -f file_name -u object_url
@@ -15,10 +17,9 @@ def download_chunk(url, start, end, part, fname, output_dir):
     filename = fname + '.chunk_%d' % part
     filepath = os.path.join(output_dir, filename)
     with open(filepath, 'wb') as f:
-        f.seek(start)
-        var = f.tell()
         f.write(r.content)
     print('Downloaded %s' % filepath)
+
 
 def main():
 
@@ -27,7 +28,7 @@ def main():
     descr = 'A command line tool that, given a single URL, can download the web object pointed by the URL by using multiple parallel TCP connections and HTTP Range requests.'
     parser = argparse.ArgumentParser(prog=prog, description=descr)
     parser.add_argument('-n', '--num_chunks', type=int, default=None, required=True, help='Number of chunks to break object into')
-    parser.add_argument('-n', '--output_dir', type=str, default=None, required=True,help='Directory in which to store chunks')
+    parser.add_argument('-o', '--output_dir', type=str, default=None, required=True,help='Directory in which to store chunks')
     parser.add_argument('-f', '--file_name', type=str, default=None, required=True, help='File name for chunks')
     parser.add_argument('-u', '--object_url', type=str, default=None, required=True,help='URL of specified object')
 
@@ -39,6 +40,10 @@ def main():
     file_name = args.file_name
     url = args.object_url
 
+    
+    #create output directory
+    os.mkdir(output_dir)
+    
     # perform a head request --> check content size and check if url accepts ranges
     r = requests.head(url)
     accepts_ranges = 'accept-ranges' in r.headers and 'bytes' in r.headers['accept-ranges']
@@ -53,8 +58,10 @@ def main():
         except:
             print('Invalid URL')
             return
+
+        print('content-length: ' , content_length)
         # compute chunk size and remainder of last chunk downloaded
-        chunk_size = content_length / num_chunks
+        chunk_size = math.floor(content_length / num_chunks)
         chunk_remainder = content_length % num_chunks
         # if there is a remainder, we have n+1 chunks total
         if chunk_remainder != 0:
@@ -66,25 +73,33 @@ def main():
             start = chunk_size*i
             part += 1
             if i != num_chunks-1:
-                end = chunk_size
+                end = start+chunk_size
             else:
-                end = chunk_remainder
+                end = start+chunk_remainder
 
+            print('Part ',part,' Start is: ',start,' end is ',end)
             t = threading.Thread(target=download_chunk, args=(url, start, end, part, file_name, output_dir))
             t.setDaemon(True)
-
             t.start()
 
+
+        while threading.active_count() > 1:
+            time.sleep(.1)
+            
         # build complete file
         filepath = os.path.join(output_dir, file_name)
         with open(filepath, 'wb') as f:
+        
             for i in range(num_chunks):
-                tmp_filename = file_name + '.chunk_%d' % i+1
-                tmp_filepath = os.path.join(output_dir, tmp_filename)
-                shutil.copyfileobj(open(tmp_filepath, 'rb'), f)
+                tmp_filename = file_name + '.chunk_%s' % str(i+1)
+                tmp_path = os.path.join(output_dir, tmp_filename)
+                shutil.copyfileobj(open(tmp_path, 'rb'), f)
+                print('Copied ' , tmp_path)
 
         print('Joining complete. File saved in %s' % filepath)
-
+        newfile = open(filepath,'rb')
+        dat = newfile.read()
+        print('SHA256 Hash: ', hashlib.sha256(dat).hexdigest())
 
 if __name__ == '__main__':
     main()
