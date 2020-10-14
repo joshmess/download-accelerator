efab71aa09b4a680ba01503b8fec1a2a0ea2d8ac
Joshua Messitte <68798981+joshmess@users.noreply.github.com>
2020-10-13 20:06:36 -0400

    PyCharm

--- downloader.py
+++ downloader.py
@@ -1,21 +1,31 @@
 import argparse
-import _thread
+import shutil
+import threading
 import urllib.request
 import requests
-
+import sys, os
 
 # Author: Josh Messitte (811976008)
 # CSCI 6760 Project 3: download-accelerator
 # Test downloader: python3 downloader.py -n num_chunks -o output_dir -f file_name -u object_url
 
 
-if __name__ == '__main__':
+def download_chunk(url, start, end, part, fname, output_dir):
+    r = requests.get(url, headers={'Range':'bytes=%d-%d' % (start, end)}, stream=True)
+    filename = fname + '.chunk_%d' % part
+    filepath = os.path.join(output_dir, filename)
+    with open(filepath, 'wb') as f:
+        f.seek(start)
+        var = f.tell()
+        f.write(r.content)
+    print('Downloaded %s' % filepath)
+
+def main():
 
     # Set up argument parsing automation
     prog = 'python3 downloader.py'
     descr = 'A command line tool that, given a single URL, can download the web object pointed by the URL by using multiple parallel TCP connections and HTTP Range requests.'
     parser = argparse.ArgumentParser(prog=prog, description=descr)
-
     parser.add_argument('-n', '--num_chunks', type=int, default=None, required=True, help='Number of chunks to break object into')
     parser.add_argument('-n', '--output_dir', type=str, default=None, required=True,help='Directory in which to store chunks')
     parser.add_argument('-f', '--file_name', type=str, default=None, required=True, help='File name for chunks')
@@ -38,7 +48,45 @@ if __name__ == '__main__':
         print('The given url does not accept byte ranges for downloading.')
     else:
         # url accepts byte ranges
-        content_length = int(r.headers['content-length'])
+        try:
+            content_length = int(r.headers['content-length'])
+        except:
+            print('Invalid URL')
+            return
+        # compute chunk size and remainder of last chunk downloaded
         chunk_size = content_length / num_chunks
         chunk_remainder = content_length % num_chunks
+        # if there is a remainder, we have n+1 chunks total
+        if chunk_remainder != 0:
+            num_chunks += 1
+
+        part = 0
+        # create a parallel threads for each chunk download
+        for i in range(num_chunks):
+            start = chunk_size*i
+            part += 1
+            if i != num_chunks-1:
+                end = chunk_size
+            else:
+                end = chunk_remainder
+
+            t = threading.Thread(target=download_chunk, args=(url, start, end, part, file_name, output_dir))
+            t.setDaemon(True)
+
+            t.start()
+
+        # build complete file
+        filepath = os.path.join(output_dir, file_name)
+        with open(filepath, 'wb') as f:
+            for i in range(num_chunks):
+                tmp_filename = file_name + '.chunk_%d' % i+1
+                tmp_filepath = os.path.join(output_dir, tmp_filename)
+                shutil.copyfileobj(open(tmp_filepath, 'rb'), f)
+
+        print('Joining complete. File saved in %s' % filepath)
+
+
+if __name__ == '__main__':
+    main()
+
 
