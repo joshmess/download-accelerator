393b3c31484871280b61a189fed52b5ba4924c0c
Joshua Messitte <68798981+joshmess@users.noreply.github.com>
2020-10-22 16:25:58 -0400

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -7,12 +7,14 @@ import urllib.parse
 import sys, os
 import time
 import socket
+import re
 
 # Author: Josh Messitte (811976008)
 # CSCI 6760 Project 3: download-accelerator
 # Test downloader: python3 downloader.py -n num_chunks -o output_dir -f file_name -u object_url
 
 port = 80
+
 
 # Thread routine that downlaods a part of the requsted object
 def download_chunk(path, host, start, end, part, fname, output_dir):
@@ -21,27 +23,23 @@ def download_chunk(path, host, start, end, part, fname, output_dir):
     sock.connect((host, port))
     byterange = str(start) + '-' + str(end)
     request = 'GET %s HTTP/1.1\r\nHost: %s\r\nRange: bytes=%s\r\n\r\n' % (path, host, byterange)
-    print('GET Request: ',request)
+    print('GET Request: ', request)
     # Send request and receive result
     sock.sendall(request.encode())
-    response = sock.recv(end - start)
-    
-    # Isolate headers
-    index = response.index(b'\r\n\r\n')
-    headers_only = response[0:index]
-    body_only = response[index+4:]
-    
-    size_of_headers = sys.getsizeof(headers_only)
+    res = b''
+    data = sock.recv(1024)
+    while data:
+        res += data
+        data = sock.recv(1024)
 
-    # Account for headers
-    new_response = sock.recv(size_of_headers)
+    # isolate content
+    content = re.findall(b'\r\n\r\n(.*)', res, re.S)
 
-    content_response = body_only+new_response
     # write file chunk to directory
     filename = fname + '.chunk_%d' % part
     filepath = os.path.join(output_dir, filename)
     with open(filepath, 'wb') as f:
-        f.write(content_response)
+        f.write(content[0])
     print('Downloaded %s' % filepath)
 
 
@@ -100,16 +98,16 @@ def main():
         part = 0
         # create n parallel threads for each chunk download
         for i in range(num_chunks):
-            #compute start, part, end
+            # compute start, part, end
             start = chunk_size * i
             part += 1
             if i != num_chunks - 1:
-                    end = start + (chunk_size-1)
-                
+                end = start + (chunk_size - 1)
+
             else:
                 end = start + chunk_size + chunk_remainder
 
-            print('Part: ',part,' Start:' , start,' End: ',end)
+            print('Part: ', part, ' Start:', start, ' End: ', end)
             # Start threads
             t = threading.Thread(target=download_chunk, args=(path, host, start, end, part, file_name, output_dir))
             t.setDaemon(True)
